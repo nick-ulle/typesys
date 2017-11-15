b6e51a300d440d9e1e710acb6dd4b278e41ade57
Nick Ulle <nick.ulle@gmail.com>
2017-11-14 19:55:05 -0800

    Rename `collect_type_vars` to `collect_variables`.

---
+++ R/collect_variables.R
@@ -0,0 +1,27 @@
+#' @include types.R
+NULL
+
+#' @export
+setGeneric("collect_variables",
+function(type) standardGeneric("collect_variables")
+)
+
+#' @export
+setMethod("collect_variables", "typesys::FunctionType",
+function(type) {
+  arg_types = lapply(type@args, collect_variables)
+  arg_types = unlist(arg_types)
+
+  union(arg_types, collect_variables(type@return_type))
+})
+
+#' @export
+setMethod("collect_variables", "typesys::TypeVar",
+function(type) type@name
+)
+
+#' @export
+setMethod("collect_variables", "typesys::AtomicType",
+function(type) character(0)
+)
+
