ce7f387fcd09f1eec5bf1c9804302fdc08f6a7c6
Nick Ulle <nick.ulle@gmail.com>
2019-05-29 20:14:46 -0700

    Add comparision operators for Terms.

---
+++ R/compare.R
@@ -0,0 +1,30 @@
+# Methods to compare terms.
+#
+
+#' @include terms.R
+#' @include types.R
+NULL
+
+setMethod("!=", c("typesys::Term", "ANY"), function(e1, e2) TRUE)
+setMethod("!=", c("ANY", "typesys::Term"), function(e1, e2) TRUE)
+setMethod("!=", c("typesys::Term", "typesys::Term"),
+  function(e1, e2) !(e1 == e2))
+
+setMethod("==", c("typesys::Term", "ANY"), function(e1, e2) FALSE)
+setMethod("==", c("ANY", "typesys::Term"), function(e1, e2) FALSE)
+setMethod("==", c("typesys::Term", "typesys::Term"), function(e1, e2) FALSE)
+
+setMethod("==", c("typesys::Variable", "typesys::Variable"),
+  function(e1, e2) {
+    identical(e1, e2)
+  })
+
+setMethod("==", c("typesys::Function", "typesys::Function"),
+  function(e1, e2) {
+    identical(e1, e2)
+  })
+
+setMethod("==", c("typesys::Constant", "typesys::Constant"),
+  function(e1, e2) {
+    identical(e1, e2)
+  })
