c140413fc6a75bc4e6ecc7b66e27c6fbd67ee5d2
Nick Ulle <nick.ulle@gmail.com>
2019-06-14 19:56:13 -0700

    Fix collation of names.R.

--- R/names.R
+++ R/names.R
@@ -1,7 +1,13 @@
-#' Variable Names
+
+#' @include substitution.R
+NULL
+
+
+#' Get Variable Names
 #'
-#' Functions to get the variable names in a Term. Names can only be set on a
-#' Variable.
+#' Methods to get the names of variables in a Term.
+#'
+#' Names can only be set on a Variable object.
 #'
 #' @param x A Term object.
 #' @return A character vector of variable names.
@@ -10,9 +16,11 @@
 #' @export
 setMethod("names", signature("typesys::Variable"), function(x) x@name)
 
+
 #' @rdname names.typesys
 #' @export
 setMethod("names", signature("typesys::Constant"), function(x) character(0))
+
 
 #' @rdname names.typesys
 #' @export
@@ -20,10 +28,6 @@ setMethod("names", signature("typesys::Composite"),
   function(x) {
     unlist(lapply(x@components, names), FALSE, FALSE)
   })
-
-# TODO: Document me.
-#' @export
-setMethod("names", "typesys::Substitution", function(x) names(x@map))
 
 
 #' @rdname names.typesys
@@ -35,3 +39,8 @@ setMethod("names<-", signature("typesys::Variable"),
 
     x
   })
+
+
+# TODO: Document me.
+#' @export
+setMethod("names", "typesys::Substitution", function(x) names(x@map))
